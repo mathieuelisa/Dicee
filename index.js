7b3e7246119b84cba29aa2dd18b57c723e1eb4bc
mathieuelisa <mathieu.elisa@gmail.com>
2020-09-04 09:51:03 +0200

    Dicee Game

---
+++ index.js
@@ -0,0 +1,35 @@
+
+// GENERAL
+
+var image = ["images/dice1.png","images/dice2.png","images/dice3.png","images/dice4.png","images/dice5.png","images/dice6.png"]
+
+// FIRST DICE
+
+var randomNumber1 = Math.floor(Math.random() * 6)
+
+var firstImage = image[randomNumber1]
+
+document.querySelector(".img1").setAttribute("src", firstImage)
+
+console.log(randomNumber1)
+
+
+// SECOND DICE
+
+var randomNumber2 = Math.floor(Math.random() * 6)
+
+var secondImage = image[randomNumber2]
+
+document.querySelector(".img2").setAttribute("src", secondImage)
+
+// WHO WIN ?
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🔥Joueur 1 Gagnant!"
+    }
+else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Joueur 2 Gagnant!🔥"
+        }
+else{
+    document.querySelector("h1").innerHTML = "Egalité!"
+}
